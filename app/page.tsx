a50c14f128cebeeffa34b9755d7c41274eada2d4
khedev <bualkhedev@gmail.com>
2025-12-05 15:35:13 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
     </section>
 
       {/* Mission Section */}
-      <section className="py-16 px-4 md:px-16 bg-black text-white">
+      <section className="items-center text-center py-16 px-4 md:px-16 bg-black text-white">
         <h2 className="text-center text-3xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent">Our Mission</h2>
         <p className="mt-6 text-white/90 text-base sm:text-lg md:text-xl leading-relaxed">
           To provide rapid, effective, and compassionate relief for communities facing disasters, ensuring they receive the resources and support needed to rebuild and thrive.
